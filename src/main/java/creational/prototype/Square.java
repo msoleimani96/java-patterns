4d910ad5eaeec23e68ed7641f93aeb46a1c0e14d
Matin Soleimani <matinsoleimani2006@gmail.com>
2025-09-06 12:40:28 +0330

    feat(prototype): design pattern sample implemented.

--- src/main/java/creational/prototype/Square.java
+++ src/main/java/creational/prototype/Square.java
@@ -1,28 +1,33 @@
 package creational.prototype;
 
+import java.util.Objects;
+
 public class Square extends Shape {
     public Square(int width) {
         super(width, width);
     }
 
     public Square(Square square) {
-        super(square.getWidth(), square.getWidth());
+        super(Objects.requireNonNull(square).getWidth(), square.getWidth());
     }
 
     @Override
     public void setWidth(int width) {
-        super.setWidth(width);
-        super.setHeight(width);
+        setSide(width);
     }
 
     @Override
     public void setHeight(int height) {
-        super.setWidth(height);
-        super.setHeight(height);
+        setSide(height);
     }
 
     @Override
     public Square clone() {
         return new Square(this);
+    }
+
+    private void setSide(int side) {
+        super.setWidth(side);
+        super.setHeight(side);
     }
 }
